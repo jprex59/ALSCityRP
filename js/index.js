144dc30043508e0ccfb3a224282be3cffcdf2b8d
jprex59 <76010227+jprex59@users.noreply.github.com>
2021-04-18 13:00:57 +0200

    responsive

--- js/index.js
+++ js/index.js
@@ -16,3 +16,24 @@ function mess2() {
 		"Pas besoins de posuler IRL dans le serveur discord, ou d'obtenir un entretien avec un directeur, les jobs présents dans le pôle emploi permettent pour les nouveaux arrivant de se faire de l'argent et commencer leur vie GTA RP. Rendez vous au pôle in-game, choissez un job et commencez votre aventure !"
 	);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+	// Get all "navbar-burger" elements
+	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+
+	// Check if there are any navbar burgers
+	if ($navbarBurgers.length > 0) {
+		// Add a click event on each of them
+		$navbarBurgers.forEach((el) => {
+			el.addEventListener("click", () => {
+				// Get the target from the "data-target" attribute
+				const target = el.dataset.target;
+				const $target = document.getElementById(target);
+
+				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+				el.classList.toggle("is-active");
+				$target.classList.toggle("is-active");
+			});
+		});
+	}
+});
