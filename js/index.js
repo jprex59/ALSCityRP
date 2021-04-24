618765a42c328d7ab200be4c14c6a4d144c77419
jprex59 <76010227+jprex59@users.noreply.github.com>
2021-04-24 22:59:44 +0200

    automatisation message en gris dans actu.html

--- js/index.js
+++ js/index.js
@@ -5,18 +5,6 @@ toggle.addEventListener("click", () => {
 	shareBtn.classList.toggle("active");
 });
 
-function mess1() {
-	alert(
-		"Disclamer : Indispo veut dire ici que le poste est complet. Si le job est libre il vous faudra monter un dossier et l'envoyer dans un ticket sur le serveur discord, les dirigeants voteront si oui ou non vous pouvez le prendre. A l'inverse, Recrute insinue que le poste de directeur affilié à ce job est déja pris,  si vous voulez faire ce job,vous ne devez plus passer par un ticket mais en vous faisant embaucher par le directeur directement."
-	);
-}
-
-function mess2() {
-	alert(
-		"Pas besoins de posuler IRL dans le serveur discord, ou d'obtenir un entretien avec un directeur, les jobs présents dans le pôle emploi permettent pour les nouveaux arrivant de se faire de l'argent et commencer leur vie GTA RP. Rendez vous au pôle in-game, choissez un job et commencez votre aventure !"
-	);
-}
-
 document.addEventListener("DOMContentLoaded", () => {
 	// Get all "navbar-burger" elements
 	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
